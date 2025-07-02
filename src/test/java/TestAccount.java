1bd7daf4e8d3e0d2f0398c47b61f1e706a28c165
tcspariwesh <tcspariwesh@gmail.com>
2025-07-02 16:10:32 +0530

    removed target forlder from remote repository

--- src/test/java/TestAccount.java
+++ src/test/java/TestAccount.java
@@ -1,4 +1,4 @@
-
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -6,18 +6,25 @@ import org.junit.rules.ExpectedException;
 import com.ust.Account;
 
 public class TestAccount {
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-    @Test
-    public void testReturnTrue() {
-        Account account = new Account();
-        boolean result = account.withdraw(200);
-        org.junit.Assert.assertEquals(true, result);
-    }
-     @Test
-    public void testThrowsException() {
-        thrown.expect(IllegalArgumentException.class);
-        Account account = new Account();
-        account.withdraw(-200);
-    }
+
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+
+  Account account;
+  @Before
+  void init() {
+    account = new Account();
+  }
+
+  @Test
+  public void testReturnTrue() {
+    boolean result = account.withdraw(200);
+    org.junit.Assert.assertEquals(true, result);
+  }
+
+  @Test
+  public void testThrowsException() {
+    thrown.expect(IllegalArgumentException.class);
+    account.withdraw(-200);
+  }
 }
