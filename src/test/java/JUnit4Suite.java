6794d63bf36cf7ccc057244d161113fae0765d43
tcspariwesh <tcspariwesh@gmail.com>
2025-07-04 15:30:38 +0530

    method sequence

--- src/test/java/JUnit4Suite.java
+++ src/test/java/JUnit4Suite.java
@@ -1,8 +1,12 @@
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({TestAccount.class, TestCalculator.class})
 public class JUnit4Suite {
-
+    @BeforeClass
+    public  static void runBeforeTestSuite(){
+        System.out.println("test suite is running");
+    }
 }
--- src/test/java/TestAccount.java
+++ src/test/java/TestAccount.java
@@ -1,5 +1,7 @@
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -9,30 +11,45 @@ import com.ust.Account;
 public class TestAccount {
 
   @Rule
-  public ExpectedException thrown = ExpectedException.none();//define a monitor
+  public ExpectedException thrown = ExpectedException.none(); //define a monitor
 
-   Account account;
+  static Account account;
 
-  @Before //use it for every time initialization per test method
-  public  void init() {
+  @BeforeClass //use it for every time initialization per test method
+  public static void initOnce() {
+    System.out.println("before class called only once");
     account = new Account();
   }
 
+  @Before //use it for every time initialization per test method
+  public void initForEveryTest() {
+    account = new Account();
+    System.out.println("before each test ");
+
+  }
+
   @After
-  public void clean() {
+  public  void clean() {
     account = null;
-    System.out.println("clean up");
+    System.out.println("clean up after each test");
   }
 
   @Test
   public void testReturnTrue() {
     boolean result = account.withdraw(200); //MUT-method under test
     org.junit.Assert.assertEquals(true, result);
+    System.out.println("testReturnTrue");
   }
 
   @Test
   public void testThrowsException() {
     thrown.expect(IllegalArgumentException.class);
     account.withdraw(-200);
+    System.out.println("testThrowsException");
+  }
+
+  @AfterClass
+  public static void cleanFinal(){
+    System.out.println("cleanFinal");
   }
 }
